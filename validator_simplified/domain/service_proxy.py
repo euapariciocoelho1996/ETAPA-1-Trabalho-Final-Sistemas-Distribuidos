779b823a92590e680ef16312f136b1715fa98578
Francisco Aparicio Nascimento Coelho <faparicionc@gmail.com>
2025-05-26 19:56:59 -0300

    update

--- validator_simplified/domain/service_proxy.py
+++
@@ -1,9 +0,0 @@
-from typing import Dict, Any, Optional
-import requests
-from .abstract_proxy import AbstractProxy
-
-class ServiceProxy(AbstractProxy):
-    def forward_request(self, method: str, endpoint: str, data: Optional[Dict[str, Any]] = None) -> Any:
-        url = f"{self.target.url}/{endpoint.lstrip('/')}"
-        response = requests.request(method, url, json=data)
-        return response.json() 
